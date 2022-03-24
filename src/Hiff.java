24023d45681c279bf8436760e6283e7134f5641e
MicroSat <MicroSat@DESKTOP-LNVMGV5>
2022-03-24 17:03:32 +0530

    kkk

---
+++ src/Hiff.java
@@ -0,0 +1,22 @@
+
+public class Hiff {
+
+	
+
+		public static void main(String args [] )
+
+		{
+
+		int a=5,b=3;
+
+		System.out.println( "Variable value is" +a );
+		System.out.println( "Variable value is" +b );
+
+
+		}
+
+		    
+
+	}
+
+
