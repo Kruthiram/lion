33ea437f9cf04f696c7fa4c8ba3a98bc80e2e39b
MicroSat <MicroSat@DESKTOP-LNVMGV5>
2022-03-24 16:59:04 +0530

    jj

---
+++ src/Sample.java
@@ -0,0 +1,22 @@
+
+public class Sample {
+
+    public void Demo ()
+
+{
+int a = 0;
+int b = a + 5;
+
+        System.out.println("B value is: " + b);
+}
+
+    public static void main (String args [])
+
+    {
+
+Sample obj = new Sample ();
+obj .Demo();
+    }
+
+
+}
