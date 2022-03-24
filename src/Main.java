cb504921e956dd4788f17db0e67cb859fe659199
MicroSat <MicroSat@DESKTOP-LNVMGV5>
2022-03-24 16:13:32 +0530

    add another value

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+class Main {
+
+public static void main(String args [] )
+
+{
+
+int a=5,b=3;
+
+System.out.println( "Variable value is" +a );
+System.out.println( "Variable value is" +b );
+
+
+}
+
+    }
